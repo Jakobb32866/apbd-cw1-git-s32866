323be6353f9afbe3c248ace44b21d6f0bb9f96bb
s32866 Jakub Szochner <s32866@pjwstk.edu.pl>
2026-03-11 09:11:35 +0100

    added the CalculateAverage method draft

--- app/app/Program.cs
+++ app/app/Program.cs
@@ -4,3 +4,8 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Mars!");
 Console.WriteLine("Hello, Jupiter!");
 Console.WriteLine("Hello, Venus!");
+
+static void CalculateAverage(int[] numbers)
+{
+    
+}
